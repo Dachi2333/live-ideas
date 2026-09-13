067c3b98f439b5ede68d9491eebaa17c1b1bbaa1
Dachi2333 <ztc.chi233@gmail.com>
2026-09-13 19:50:50 +0900

    feat: render swipe-delete fragment rows

--- src/client/render.js
+++ src/client/render.js
@@ -7,10 +7,13 @@ export function escapeHtml(text) {
     .replaceAll("'", "&#39;");
 }
 
+const trashIcon = `<svg class="trash-icon" viewBox="0 0 24 24" aria-hidden="true"><path d="M4.5 6.5h15M9 6.5V4.2h6v2.3M7 6.5l1 13h8l1-13M10 10v6.5M14 10v6.5" /></svg>`;
+
 export function renderFragments(fragments, { formatTime }) {
   return fragments.map((fragment) => {
+    const id = escapeHtml(fragment.id);
     const text = escapeHtml(fragment.text).replaceAll("\n", "<br>");
     const time = escapeHtml(formatTime(fragment.sentAt));
-    return `<article class="fragment-row"><div class="fragment-text">${text}</div><time>${time}</time></article>`;
+    return `<article class="fragment-shell" data-fragment-id="${id}"><button class="delete-zone" type="button" aria-label="Delete fragment">${trashIcon}</button><div class="fragment-row"><div class="fragment-text">${text}</div><time>${time}</time></div></article>`;
   }).join("");
 }
